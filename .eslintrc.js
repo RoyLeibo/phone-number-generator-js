0818211fc2da6cb538c25bd54ed52c6cef657e51
royleibovitz <royleibo212@gmail.com>
2025-12-04 15:02:32 +0200

    fix: Simplify ESLint config to remove prettier integration
    - Remove prettier plugin from ESLint to avoid CI issues
    - Keep TypeScript linting rules
    - Prettier can still be used separately if needed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended'],
   parserOptions: {
     sourceType: 'module',
   },
-  plugins: ['prettier'],
   rules: {
-    'prettier/prettier': 'error',
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-non-null-assertion': 'warn',
