381aa96665eecfd6171b5454d2c1133121a85293
royleibovitz <royleibo212@gmail.com>
2025-12-04 14:52:07 +0200

    fix: Add prettier plugin to ESLint config and adjust rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
   parserOptions: {
     sourceType: 'module',
   },
+  plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
   },
 }
